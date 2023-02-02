ad60b8905ee9b31cdf3c6c548474befb5f420365
Ryo Fujiwara <jjcproject@icloud.com>
2023-02-02 17:52:55 +0900

    json file merge code

--- src/hairSurvey.js
+++ src/hairSurvey.js
@@ -5,7 +5,38 @@ import { addDoc, collection, getFirestore } from "firebase/firestore";
 import jsonData from "./data/recommendItem.json";
 import filteredData from "./data/itemList.json";
 
-console.log(jsonData.recommendedItem.length)
+
+const mappedjsondata = jsonData.recommendedItem.map((item) => {
+  const item1info = filteredData.itemList.find((item2) => item2.FullProductName === item.recItem1)
+  const item2info = filteredData.itemList.find((item2) => item2.FullProductName === item.recItem2)
+  const item3info = filteredData.itemList.find((item2) => item2.FullProductName === item.recItem3)
+  return {
+    patternID: item.patternID,
+    scalpType: item.scalpType,
+    damageType:item.damageType,
+    hairType: item.hairType,
+    finishType: item.finishType,
+    recItem1: item.recItem1,
+    recItem1info: item1info || {},
+    recItem2: item.recItem2,
+    recItem2info: item2info || {},
+    recItem3: item.recItem3,
+    recItem3info : item3info || {},
+}
+});
+console.log(mappedjsondata);
+// console.log(filteredData.itemList[8].FullProductName);
+
+// console.log(jsonData.recommendedItem[0].recItem1);
+
+// if(filteredData.itemList[8].FullProductName === jsonData.recommendedItem[0].recItem1) {
+//   console.log(filteredData.itemList[8].FullProductName,":",filteredData.itemList[8].itemDescription);
+// } 
+
+// var itemListNum = filteredData.itemList.length - 1
+// console.log(itemListNum);
+
+// for(let i=0; i < itemListNum )
 
 var uuid = require("uuid");
 
@@ -15,7 +46,7 @@ const firebaseConfig = {
   projectId: "hair-survey-app",
   storageBucket: "hair-survey-app.appspot.com",
   messagingSenderId: "526490795482",
-  appId: "1:526490795482:web:26163607e7c57ac99854e3"
+  appId: "1:526490795482:web:26163607e7c57ac99854e3",
 };
 
 const app = initializeApp(firebaseConfig);
@@ -44,34 +75,39 @@ class HairSurvey extends Component {
   }
 
   async surveySubmit(event) {
-    event.preventDefault()
-    const doc = await addDoc(collection(db, 'responses'),{
-        uid: this.state.uid,
-        studentName: this.state.studentName,
-        answers: this.state.answers
+    event.preventDefault();
+    const doc = await addDoc(collection(db, "responses"), {
+      uid: this.state.uid,
+      studentName: this.state.studentName,
+      answers: this.state.answers,
     });
 
-    console.log("new document added: ", doc.id)
-
-    const filteredItems = jsonData.recommendedItem.filter(item => {
+    console.log("new document added: ", doc.id);
+    
+    const filteredItems = mappedjsondata.filter((item) => {
       const { ans1, ans2, ans3, ans4 } = this.state.answers;
-      const { scalpType, damageType, hairType, finishType } = item
+      const { scalpType, damageType, hairType, finishType } = item;
 
-      if(ans1 === scalpType && ans2 === damageType && ans3 === hairType && ans4 === finishType) {
-        return true
+      if (
+        ans1 === scalpType &&
+        ans2 === damageType &&
+        ans3 === hairType &&
+        ans4 === finishType
+      ) {
+        return true;
       }
-    })
+    });
 
     // const getItem = filteredData.itemList.map(product => {
-      
+
     //   var productList = [];
     //   const { FullProductName } = product
     //   const { recItem1, rectItem2, recItem3 } = product
     //   productList.push(product)
-      
-    // })  
 
-    console.log(jsonData.recommendedItem.length, filteredItems.length);
+    // })
+
+    console.log(mappedjsondata.length, filteredItems.length);
 
     this.setState({ isSubmitted: true, filteredItems });
   }
@@ -116,24 +152,26 @@ class HairSurvey extends Component {
     // jan.11th
     var name = "";
     var questions = "";
-    var result = ""
+    var result = "";
 
     if (this.state.studentName === "" && this.state.isSubmitted === false) {
       name = (
         <div className="top">
           <div className="intro">
-          <h1><img src={images["logo02.svg"]} alt="KEVIN.MURPHY"/></h1>
-          <h2>ヘアケア診断</h2>
-          <p>お客様の悩みに合う商品をお勧めします。</p>
-          <h4>名前を入力してください。</h4>
-          <form onSubmit={this.studentNameSubmit}>
-            <input
-              className="sName"
-              type="text"
-              placeholder="名前を入力"
-              ref="name"
-            />
-          </form>
+            <h1>
+              <img src={images["logo02.svg"]} alt="KEVIN.MURPHY" />
+            </h1>
+            <h2>ヘアケア診断</h2>
+            <p>お客様の悩みに合う商品をお勧めします。</p>
+            <h4>名前を入力してください。</h4>
+            <form onSubmit={this.studentNameSubmit}>
+              <input
+                className="sName"
+                type="text"
+                placeholder="名前を入力"
+                ref="name"
+              />
+            </form>
           </div>
         </div>
       );
@@ -160,26 +198,35 @@ class HairSurvey extends Component {
                     name="ans1"
                     value="normal"
                     onChange={this.answerSelected}
+                    id="m"
                   />
-                  <img alt="KEVIN.MURPHY" src={images["scalpNormal.png"]} />
+                  <label for="m">
+                    <img alt="KEVIN.MURPHY" src={images["scalpNormal.png"]} />
+                  </label>
                 </div>
                 <div className="answer">
                   <input
                     type="radio"
                     name="ans1"
-                    value="ドライ"
+                    value="dry"
                     onChange={this.answerSelected}
+                    id="l"
                   />
-                  <img alt="KEVIN.MURPHY" src={images["scalpDry.png"]} />
+                  <label for="l">
+                    <img alt="KEVIN.MURPHY" src={images["scalpDry.png"]} />
+                  </label>
                 </div>
                 <div className="answer">
                   <input
                     type="radio"
                     name="ans1"
-                    value="オイリー"
+                    value="oily"
                     onChange={this.answerSelected}
+                    id="k"
                   />
-                  <img alt="KEVIN.MURPHY" src={images["scalpOily.png"]} />
+                  <label for="k">
+                    <img alt="KEVIN.MURPHY" src={images["scalpOily.png"]} />
+                  </label>
                 </div>
               </div>
             </div>
@@ -193,8 +240,11 @@ class HairSurvey extends Component {
                     name="ans2"
                     value="low"
                     onChange={this.answerSelected}
+                    id="j"
                   />
-                  <img alt="KEVIN.MURPHY" src={images["img004-low.png"]} />
+                  <label for="j">
+                    <img alt="KEVIN.MURPHY" src={images["img004-low.png"]} />
+                  </label>
                 </div>
                 <div className="answer">
                   <input
@@ -202,8 +252,11 @@ class HairSurvey extends Component {
                     name="ans2"
                     value="middle"
                     onChange={this.answerSelected}
+                    id="i"
                   />
-                  <img alt="KEVIN.MURPHY" src={images["img005-middle.png"]} />
+                  <label for="i">
+                    <img alt="KEVIN.MURPHY" src={images["img005-middle.png"]} />
+                  </label>
                 </div>
                 <div className="answer">
                   <input
@@ -211,8 +264,11 @@ class HairSurvey extends Component {
                     name="ans2"
                     value="high"
                     onChange={this.answerSelected}
+                    id="h"
                   />
-                  <img alt="KEVIN.MURPHY" src={images["img006-high.png"]} />
+                  <label for="h">
+                    <img alt="KEVIN.MURPHY" src={images["img006-high.png"]} />
+                  </label>
                 </div>
               </div>
             </div>
@@ -226,9 +282,12 @@ class HairSurvey extends Component {
                     name="ans3"
                     value="curly"
                     onChange={this.answerSelected}
+                    id="g"
                   />
-                  クセ毛
-                  <img alt="KEVIN.MURPHY" src={images["img007-curly.png"]} />
+                  <label for="g">
+                    クセ毛
+                    <img alt="KEVIN.MURPHY" src={images["img007-curly.png"]} />
+                  </label>
                 </div>
                 <div className="answer">
                   <input
@@ -236,9 +295,12 @@ class HairSurvey extends Component {
                     name="ans3"
                     value="thin"
                     onChange={this.answerSelected}
+                    id="f"
                   />
-                  細・軟
-                  <img alt="KEVIN.MURPHY" src={images["img008-thin.png"]} />
+                  <label for="f">
+                    細・軟
+                    <img alt="KEVIN.MURPHY" src={images["img008-thin.png"]} />
+                  </label>
                 </div>
                 <div className="answer">
                   <input
@@ -246,9 +308,12 @@ class HairSurvey extends Component {
                     name="ans3"
                     value="hard"
                     onChange={this.answerSelected}
+                    id="e"
                   />
-                  硬・多
-                  <img alt="KEVIN.MURPHY" src={images["img009-thick.png"]} />
+                  <label for="e">
+                    硬・多
+                    <img alt="KEVIN.MURPHY" src={images["img009-thick.png"]} />
+                  </label>
                 </div>
               </div>
             </div>
@@ -262,8 +327,9 @@ class HairSurvey extends Component {
                     name="ans4"
                     value="scalp"
                     onChange={this.answerSelected}
+                    id="a"
                   />
-                  頭皮ケア
+                  <label for="a">頭皮ケア</label>
                 </div>
                 <div className="answer last">
                   <input
@@ -271,8 +337,9 @@ class HairSurvey extends Component {
                     name="ans4"
                     value="touch"
                     onChange={this.answerSelected}
+                    id="b"
                   />
-                  質感重視
+                  <label for="b">質感重視</label>
                 </div>
                 <div className="answer last">
                   <input
@@ -280,8 +347,9 @@ class HairSurvey extends Component {
                     name="ans4"
                     value="style"
                     onChange={this.answerSelected}
+                    id="c"
                   />
-                  スタイル
+                  <label for="c">スタイル</label>
                 </div>
                 <div className="answer last">
                   <input
@@ -289,8 +357,9 @@ class HairSurvey extends Component {
                     name="ans4"
                     value="damage"
                     onChange={this.answerSelected}
+                    id="d"
                   />
-                  ダメージケア
+                  <label for="d">ダメージケア</label>
                 </div>
               </div>
             </div>
@@ -302,28 +371,28 @@ class HairSurvey extends Component {
     }
 
     if (this.state.isSubmitted) {
-        result = (
-          <div className="survey-Answers">
-            <div className="list">
+      result = (
+        <div className="survey-Answers">
+          <div className="list">
             <div className="successful">
-            
-            <h3>完了</h3>
-            
-            <p>
-            ありがとうございます。<br></br>
-            こちらがお勧め商品となります。</p>
+              <h3>完了</h3>
+
+              <p>
+                ありがとうございます。<br></br>
+                こちらがお勧め商品となります。
+              </p>
             </div>
-            {this.state.filteredItems.map(item => (
+            {this.state.filteredItems.map((item) => (
               <div className="itemList" key={item.patternID}>
                 <div className="item">
                   <h4>{item.recItem1}</h4>
                   <div className="item-image">
                     <div className="overlay">
                       <div className="description">
-                      {item.recItem1description}
+                        {item.recItem1info.itemDescription}
                       </div>
                     </div>
-                    <img src={item.recItem1img} alt="KEVIN.MURPHY"/>
+                    <img src={item.recItem1info.itemImage} alt="KEVIN.MURPHY" />
                   </div>
                 </div>
                 <div className="item">
@@ -331,10 +400,10 @@ class HairSurvey extends Component {
                   <div className="item-image">
                     <div className="overlay">
                       <div className="description">
-                      {item.recItem2description}
+                      {item.recItem2info.itemDescription}
                       </div>
                     </div>
-                    <img src={item.recItem2img} alt="KEVIN.MURPHY"/>
+                    <img src={item.recItem2info.itemImage} alt="KEVIN.MURPHY" />
                   </div>
                 </div>
                 <div className="item">
@@ -342,18 +411,17 @@ class HairSurvey extends Component {
                   <div className="item-image">
                     <div className="overlay">
                       <div className="description">
-                      {item.recItem3description}
+                      {item.recItem3info.itemDescription}
                       </div>
                     </div>
-                    <img src={item.recItem3img} alt="KEVIN.MURPHY"/>
+                    <img src={item.recItem3info.itemImage} alt="KEVIN.MURPHY" />
                   </div>
                 </div>
               </div>
             ))}
-            </div>
           </div>
-        )
-      
+        </div>
+      );
     }
 
     return (
